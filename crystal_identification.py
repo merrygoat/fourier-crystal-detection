e493ada7297170f0c017ff51ad6ec1a9001e130c
Peter <merrygoat@users.noreply.github.com>
2018-05-23 18:10:02 +0100

    Code runing in Python 3

--- crystal_identification.py
+++ crystal_identification.py
@@ -5,19 +5,21 @@ import matplotlib
 from skimage.feature import peak_local_max
 
 
-def fourieronregion(region):
+def get_fourier_transform_of_slice(region):
     # returns the fourier transform of a region
     fourier = np.log10(np.fft.fftshift(abs(np.fft.fft2(region))))
     return fourier
 
 
-def cutfourier(fourier, fourier_cut_low):
-    # crops the fourier transform to region that contains peaks, for peak detection
-    fourier_cut_high = (1 - fourier_cut_low)
-    fourier_central = fourier[
-                      fourier.shape[0] * fourier_cut_low:fourier.shape[0] * fourier_cut_high,
-                      fourier.shape[1] * fourier_cut_low:fourier.shape[1] * fourier_cut_high]
-    return fourier_central
+def crop_image(image, cut_size):
+    # crops an array
+    cut_high = (1 - cut_size)
+    x_min = int(image.shape[0] * cut_size)
+    x_max = int(image.shape[0] * cut_high)
+    y_min = int(image.shape[1] * cut_size)
+    y_max = int(image.shape[1] * cut_high)
+    cropped_image = image[x_min:x_max, y_min:y_max]
+    return cropped_image
 
 
 def findmaxima(fourier_central, threshold, min_distance):
@@ -56,52 +58,65 @@ def ringintensity(fourier_cut, maxima, number_ring_samples):
     return intensityratio
 
 
-def scanfourier(im, threshold, size_scan_box,
-                ring_threshold):  # scans a box along the original image, and uses the foruier transform in that box to assign a number to whether it's a crystal or a liquid
+def scanfourier(original_image, threshold, size_of_scan_box, ring_threshold):
+    # scans a box along the original image, and uses the foruier transform in that box to assign a number to whether it's a crystal or a liquid
 
-    size_of_small_box = 4
-    size_of_scan_box = size_scan_box
+    rastering_interval = 4  # Rastering size
     # when we take fourier transform of the scan box, crop it to highlight crystal peaks
     fourier_cut_low = 0.3
 
-    dimension_results_arr = (im.shape[0] - size_of_scan_box) / size_of_small_box
-    results_array = np.empty((dimension_results_arr, dimension_results_arr))
+    num_x_rasters = int((original_image.shape[0] - size_of_scan_box) / rastering_interval)
+    num_y_rasters = int((original_image.shape[1] - size_of_scan_box) / rastering_interval)
+    results_array = np.empty((num_x_rasters, num_y_rasters))
 
-    for lowestx in np.arange(0, im.shape[0] - size_of_scan_box, size_of_small_box):
+    for lowestx in np.arange(0, original_image.shape[0] - size_of_scan_box, rastering_interval):
         # scanning over input image
-        for lowesty in np.arange(0, im.shape[0] - size_of_scan_box, size_of_small_box):
+        for lowesty in np.arange(0, original_image.shape[1] - size_of_scan_box, rastering_interval):
             # take the relevant slice
-            scan_box = im[lowestx:lowestx + size_of_scan_box, lowesty:lowesty + size_of_scan_box]
-            fourier = fourieronregion(scan_box)
-            fourier_central = cutfourier(fourier, fourier_cut_low)
+            image_subsection = original_image[lowestx:lowestx + size_of_scan_box, lowesty:lowesty + size_of_scan_box]
+            ft_of_subsection = get_fourier_transform_of_slice(image_subsection)
+            cropped_ft_of_subsection = crop_image(ft_of_subsection, fourier_cut_low)
 
-            min_distance = fourier.shape[0] / 20.
-            maxima = findmaxima(fourier_central, threshold, min_distance)
+            min_distance = int(ft_of_subsection.shape[0] / 20.)
+            maxima = findmaxima(cropped_ft_of_subsection, threshold, min_distance)
 
+            subsection_index = [int(lowestx / rastering_interval), int(lowesty / rastering_interval)]
             # if we've found some peaks
             if maxima.shape[0] > 0:
                 # calculate whether we have a ring
-                ring_intensity = ringintensity(fourier_central, maxima, 100)
+                ring_intensity = ringintensity(cropped_ft_of_subsection, maxima, 100)
                 if maxima.shape[0] == 6:
                     # clear hexagonal crystal
                     if ring_intensity < ring_threshold:
-                        results_array[lowestx / size_of_small_box, lowesty / size_of_small_box] = 6
+                        results_array[subsection_index[0], subsection_index[1]] = 6
                     else:
                         # potential boundary between hex+liquid
-                        results_array[lowestx / size_of_small_box, lowesty / size_of_small_box] = 1
+                        results_array[subsection_index[0], subsection_index[1]] = 1
                 elif maxima.shape[0] == 4:
                     if ring_intensity < ring_threshold:  # clear cross crystal
-                        results_array[lowestx / size_of_small_box, lowesty / size_of_small_box] = 4
+                        results_array[subsection_index[0], subsection_index[1]] = 4
                     else:
                         # potential boundary between cross+liquid
                         results_array[
-                            lowestx / size_of_small_box, lowesty / size_of_small_box] = 0.5
+                            subsection_index[0], subsection_index[1]] = 0.5
                 else:
-                    results_array[lowestx / size_of_small_box, lowesty / size_of_small_box] = 0
+                    results_array[subsection_index[0], subsection_index[1]] = 0
             else:
-                results_array[lowestx / size_of_small_box, lowesty / size_of_small_box] = 0
+                results_array[subsection_index[0], subsection_index[1]] = 0
 
     return results_array
+
+
+def plot_result(array):
+    ###plotting###
+    cmap = matplotlib.colors.ListedColormap(
+        ['darkblue', 'skyblue', 'cadetblue', 'limegreen', 'orangered'])
+    # plot, 6 = hexagonal crystal, 4 = cross crystal 1.0 = hex+liquid ring, 0.5 = cross + liquid ring, 0.0 = liquid (neither 4 nor 6 peaks detected)
+    bounds = [-0.4, 0.4, 0.6, 1.4, 5.9, 6.1]
+    norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+    fig = plt.imshow(array, interpolation='nearest', cmap=cmap, norm=norm)
+    plt.colorbar(fig, cmap=cmap, norm=norm, boundaries=bounds, ticks=[0, 0.5, 1, 4, 6])
+    plt.savefig("crystal_measure.png")
 
 
 def main():
@@ -110,13 +125,8 @@ def main():
 
     array = scanfourier(im, 0.5, 128, 0.9)
 
-    ###plotting###
-    cmap = matplotlib.colors.ListedColormap(
-        ['darkblue', 'skyblue', 'cadetblue', 'limegreen', 'orangered'])
-    # plot, 6 = hexagonal crystal, 4 = cross crystal 1.0 = hex+liquid ring, 0.5 = cross + liquid ring, 0.0 = liquid (neither 4 nor 6 peaks detected)
-    bounds = [-0.4, 0.4, 0.6, 1.4, 5.9, 6.1]
-    norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+    plot_result(array)
 
-    fig = plt.imshow(array, interpolation='nearest', cmap=cmap, norm=norm)
-    plt.colorbar(fig, cmap=cmap, norm=norm, boundaries=bounds, ticks=[0, 0.5, 1, 4, 6])
-    plt.show()
+
+if __name__ == '__main__':
+    main()
